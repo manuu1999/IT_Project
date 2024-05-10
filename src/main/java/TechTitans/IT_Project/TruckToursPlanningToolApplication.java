d47d80568e9cc89fd2e71712bdaa297c477b7c71
manuu1999 <manuel.buser@students.fhnw.ch>
2024-05-10 11:55:05 +0200

    Initial commit

---
+++ src/main/java/TechTitans/IT_Project/TruckToursPlanningToolApplication.java
@@ -0,0 +1,13 @@
+package TechTitans.IT_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckToursPlanningToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckToursPlanningToolApplication.class, args);
+	}
+
+}
